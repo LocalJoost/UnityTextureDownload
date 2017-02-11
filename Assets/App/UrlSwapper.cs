7b55fe86ece3e21dcdc32abdd889a2a2fcbdeb12
Joost van Schaik <joostvanschaik@outlook.com>
2017-02-11 17:37:13 +0100

    Initial checkin

---
+++ Assets/App/UrlSwapper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class UrlSwapper : MonoBehaviour, IInputClickHandler
+{
+    private List<DynamicTextureDownloader> _textureDownloaders;
+
+    private List<DynamicTextureDownloader> GetTextureDownloaders()
+    {
+        if (_textureDownloaders == null)
+        {
+            _textureDownloaders = new List<DynamicTextureDownloader>();
+            foreach (Transform gameObj in transform.parent.transform)
+            {
+                _textureDownloaders.Add(gameObj.GetComponent<DynamicTextureDownloader>());
+            }
+        }
+        return _textureDownloaders;
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        var tmpUrl = GetTextureDownloaders()[0].ImageUrl;
+        GetTextureDownloaders()[0].ImageUrl = GetTextureDownloaders()[1].ImageUrl;
+        GetTextureDownloaders()[1].ImageUrl = tmpUrl;
+    }
+}
